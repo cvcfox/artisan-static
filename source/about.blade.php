2faede96c573a7271e8ae7158b425b6067d3945f
cvcfox <>
2021-10-10 10:38:18 -0400

    Added changes.

--- source/about.blade.php
+++ source/about.blade.php
@@ -6,10 +6,8 @@
     <h1>About</h1>
 
     <p>My name is {{ $page->owner->name }}</p>
+    <img src="https://cjamesfox.com/img/IMG_5218.jpg" alt="" />
 
-    <h2>Links:</h2>
+	<a href="https://instagram.com/cjamesfox" target="_blank">Instagram</a>
 
-    <ul>
-        <li><a href="https://instagram.com/cjamesfox" target="_blank">Instagram</a></li>
-    </ul>
 @endsection
--- source/portfolio.blade.php
+++ source/portfolio.blade.php
@@ -3,6 +3,7 @@
 @section('title', 'Portfolio')
 
 @section('content')
+	<a href="https://instagram.com/cjamesfox" target="_blank">Instagram</a><br/>
 	<img src="https://cjamesfox.com/img/FOX_2020.jpg" alt="" />
 	<img src="https://cjamesfox.com/img/FOX_2843.jpg" alt="" />
 	<img src="https://cjamesfox.com/img/FOX_2030.jpg" alt="" />
