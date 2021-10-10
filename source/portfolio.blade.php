6da4e4943af37cf50ac5c1dee1198ebeed0666ea
cvcfox <>
2021-10-10 10:31:38 -0400

    Added changes.

--- source/portfolio.blade.php
+++ source/portfolio.blade.php
@@ -3,11 +3,16 @@
 @section('title', 'Portfolio')
 
 @section('content')
-
-<div class="container">
-  <div class="image-wrapper">
-    <img src="img/DSC_0019.jpg" alt="" />
-  </div>
-</div>
-
+    <img src="https://cjamesfox.com/img/DSC_0019.jpg" alt="" />
+	<img src="https://cjamesfox.com/img/FOX_2020.jpg" alt="" />
+	<img src="https://cjamesfox.com/img/FOX_2843.jpg" alt="" />
+	<img src="https://cjamesfox.com/img/FOX_2030.jpg" alt="" />
+	<img src="https://cjamesfox.com/img/FOX_2047.jpg" alt="" />
+	<img src="https://cjamesfox.com/img/FOX_2179.jpg" alt="" />
+	<img src="https://cjamesfox.com/img/FOX_2216.jpg" alt="" />
+	<img src="https://cjamesfox.com/img/FOX_2244.jpg" alt="" />
+	<img src="https://cjamesfox.com/img/FOX_2300.jpg" alt="" />
+	<img src="https://cjamesfox.com/img/FOX_2325.jpg" alt="" />
+	<img src="https://cjamesfox.com/img/FOX_2373.jpg" alt="" />
+	<img src="https://cjamesfox.com/img/FOX_2439.jpg" alt="" />
 @endsection
