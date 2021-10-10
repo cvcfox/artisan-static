69c8b888c512aef1f68efb421f415db1a54ec4cd
cvcfox <>
2021-10-10 11:21:30 -0400

    Added changes.

--- source/about.blade.php
+++ source/about.blade.php
@@ -4,9 +4,12 @@
 
 @section('content')
     <h1>About</h1>
-    <img src="https://cjamesfox.com/img/IMG_5218.jpg" alt="" style="width: 33%; height: 33%" />
-    <p>My name is {{ $page->owner->name }}. Software developer by day, photographer by night.  I offer an affordable option for those who don't need the "premium extras".</p>
-
-
-
+    <div style="width: 100%;">
+	    <div style="width: 50%; float: left;"> 
+	   		<img src="https://cjamesfox.com/img/IMG_5218.jpg" alt="" style="width: 33%; height: 33%" />
+	    </div>
+	    <div style="width: 50%; float: right;"> 
+	    	<p>My name is {{ $page->owner->name }}. Software developer by day, photographer by night.  I offer an affordable option for those who don't need the "premium extras".</p>
+		</div>
+	</div>
 @endsection
