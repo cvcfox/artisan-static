b36c1f8d04e3520f9427944db7ba56cabcd63c12
cvcfox <>
2021-10-10 11:31:58 -0400

    Added changes.

--- source/about.blade.php
+++ source/about.blade.php
@@ -6,7 +6,7 @@
     <h1>About</h1>
     <div style="display: flex; ">
 	    <div style="width: 50%;"> 
-	   		<img src="https://cjamesfox.com/img/IMG_5218.jpg" alt="" style="width: 33%; height: 200px" />
+	   		<img src="https://cjamesfox.com/img/IMG_5218.jpg" alt="" style="height: 250px" />
 	    </div>
 	    <div style="flex-grow: 1;"> 
 	    	<p>My name is {{ $page->owner->name }}. Software developer by day, photographer by night.  I offer an affordable option for those who don't need the "premium extras".</p>
