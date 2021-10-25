557fc9984e0eb87ae19f170a281a99649c94b353
cvcfox <>
2021-10-24 22:02:14 -0400

    Added changes.

--- source/rates.blade.php
+++ source/rates.blade.php
@@ -5,22 +5,51 @@
 @section('content')
     <h1>Rates</h1>
 
-	<h3 class="rate-package">Portrait Session Package: $350</h3>
-	<blockquote class="rate-quote">Perfect for families, couples&nbsp;&amp;&nbsp;children</blockquote>
-
+	<h3 class="rate-package">Portrait Session Package: <span class="rate-price">$350</span></h3>
+	<blockquote class="rate-quote">Perfect for families, couples&nbsp;&amp;&nbsp;children.</blockquote>
+	This package includes:
 	<ul>
         <li>All high-resolution images<br>
         <span>(average of 30 images)</span></li>
-    </ul>
-    
-    <h3 class="all-include">All Packages Include:</h3>
-   	<ul>
-        <li>Photographer's time and talent at the session (up to 60 minutes)</li>
+        <li>1 hour photography session</li>
         <li>Travel within a 30 kms radius of downtown Ottawa<br>
         <span>(please contact me to inquire about out-of-town rates)</span></li>
-        <li>Session gallery presented to you within 7 days</li>
         <li>Digital images are provided via download link only<br>
         <span>(in high-resolution and convenient web-sized formats)</span></li>
+    </ul>
+    
+    <h3 class="rate-package">Wedding Package 1: <span class="rate-price">$1800</span></h3>
+	<blockquote class="rate-quote">Perfect if you want to capture the iconic scenes of your special day: getting ready, pre-ceremony scenes of you and the wedding party, the ceremony, the formal portraits of you and your family,  the dinner and speeches.</blockquote>
+	This package includes:
+	<ul>
+        <li>All high-resolution images<br></li>
+        <li>8-hours of wedding day coverage</li>
+        <li>Private Online Gallery</li>
+        <li>Digital images are provided via download link only<br>
+        <span>(prints can be requested)</span></li>
+    </ul>
+    
+    <h3 class="rate-package">Wedding Package 2: <span class="rate-price">$2200</span></h3>
+	<blockquote class="rate-quote">With package 2, you can document the happy memories of your wedding day with an additional two hours of coverage. No moments or guests will be left out or missed.</blockquote>
+	This package includes:
+	<ul>
+        <li>All high-resolution images<br></li>
+        <li>10-hours of wedding day coverage</li>
+        <li>Private Online Gallery</li>
+        <li>Digital images are provided via download link only<br>
+        <span>(prints can be requested)</span></li>
+    </ul>
+    
+    <h3 class="rate-package">Wedding Package 3: <span class="rate-price">$3000</span></h3>
+	<blockquote class="rate-quote">Package 3 will have every moment and every detail of your beautiful day captured. It will document your story from the moment you get ready until the end of the last song.</blockquote>
+	This package includes:
+	<ul>
+        <li>All high-resolution images<br></li>
+        <li>12-hours of wedding day coverage</li>
+        <li>Engagement session</li>
+        <li>Private Online Gallery</li>
+        <li>Digital images are provided via download link only<br>
+        <span>(prints can be requested)</span></li>
     </ul>
    
 	<h3 class="fine-print">Fine Print:</h3>
