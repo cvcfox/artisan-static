1166944efebe2f9e3c01fa20c93925874b75767f
cvcfox <>
2021-10-24 22:12:06 -0400

    Added changes.

--- source/index.blade.php
+++ source/index.blade.php
@@ -7,10 +7,10 @@
     <h3>Mission Statements</h3>
 	
 	<h4>1. Provide Exceptional Photos</h4>
-	<p>The first statement might seem like the most obvious, but it should be said.  Offering quality photos to clients provides them with memories that can last a lifetime.</p> 
+	<p class="mission-statement">The first statement might seem like the most obvious, but it should be said.  Offering quality photos to clients provides them with memories that can last a lifetime.</p> 
 	<h4>2. Provide Value</h4>
-	<p>Balancing between price and service.  With pre-photo shoot discussions, cjamesfox photography offers client's needs without sacrificing what they value most. Require more than the average number of photos? On a budget?</p>
+	<p class="mission-statement">Balancing between price and service.  With pre-photo shoot discussions, cjamesfox photography offers client's needs without sacrificing what they value most. Require more than the average number of photos? On a budget?</p>
 	<h4>3. Building Trust</h4>
-	<p>Communicating, being honest and open with clients helps build trust.  Having a channel open at all times and setting a deadline creates an easy stress free process.</p>
+	<p class="mission-statement">Communicating, being honest and open with clients helps build trust.  Having a channel open at all times and setting a deadline creates an easy stress free process.</p>
 	
 @endsection
