98cf4d6800c4f0b7cc933b4870905dacf1ec18b9
cvcfox <>
2021-10-24 21:52:13 -0400

    Added changes.

--- source/rates.blade.php
+++ source/rates.blade.php
@@ -5,6 +5,32 @@
 @section('content')
     <h1>Rates</h1>
 
+	<h3 class="rate-package">Portrait Session Package: $350</h3>
+	<blockquote class="rate-quote">Perfect for families, couples&nbsp;&amp;&nbsp;children</blockquote>
 
+	<ul>
+        <li>All high-resolution images<br>
+        <span>(average of 30 images)</span></li>
+    </ul>
+    
+    <h3 class="all-include">All Packages Include:</h3>
+   	<ul>
+        <li>Photographer's time and talent at the session (up to 60 minutes)</li>
+        <li>Travel within a 30 kms radius of downtown Ottawa<br>
+        <span>(please contact me to inquire about out-of-town rates)</span></li>
+        <li>Session gallery presented to you within 7 days</li>
+        <li>Digital images are provided via download link only<br>
+        <span>(in high-resolution and convenient web-sized formats)</span></li>
+    </ul>
+   
+	<h3 class="fine-print">Fine Print:</h3>
+	<ul>
+	    <li>Payment can be made by banking e-transfer (Interac) or credit card via Paypal</li>
+	    <li>All packages are for one session date and location</li>
+	    <li>All pricing is non-negotiable and is subject to change at any time unless the fee has been paid.</li>
+	   	<li>The fee is due at time of booking and secures your session date.</li>
+	    <li>The fee is non-refundable, but it is transferable in the event that you must re-schedule (24 hours notice required)</li>
+	    <li>Due to the custom nature of photography, all sales are final</li>
+	</ul>
 
 @endsection
